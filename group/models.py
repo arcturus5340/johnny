6b7defb9dbc5334bf6fc7887e430d6d78d63a404
arcturus5340 <arcturus5340@gmail.com>
2020-07-26 23:40:32 +0200

    add model with groups members

--- group/models.py
+++ group/models.py
@@ -1,1 +1,25 @@
 from django.db import models
+from django.utils import timezone
+
+
+class Members(models.Model):
+    user = models.IntegerField(
+        verbose_name='ID Пользователя'
+    )
+    chat = models.IntegerField(
+        verbose_name='ID Группы'
+    )
+    date_joined = models.DateTimeField(default=timezone.now)
+    rating = models.IntegerField(
+        default=0,
+        verbose_name='Репутация'
+    )
+    mute_rating = models.IntegerField(
+        default=0,
+        verbose_name='Кол-во нарушений'
+    )
+
+    class Meta:
+        verbose_name = 'Пользователь'
+        verbose_name_plural = 'Пользователи'
+
