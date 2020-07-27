65e4424fb0e82e53c4eb775742d504a743789a63
arcturus5340 <arcturus5340@gmail.com>
2020-07-27 23:48:32 +0200

    delete bot token from settings file

--- johnny/settings.py
+++ johnny/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.admin',
+    'group.apps.GroupConfig',
 ]
 
 MIDDLEWARE = [
@@ -103,7 +104,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ru'
 
 TIME_ZONE = 'UTC'
 
@@ -118,7 +119,3 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-
-# Bot settings
-
-TOKEN = '637788001:AAHeTOwJS5e0mpDyPcJDJZpfcn9-X96e-4M'
