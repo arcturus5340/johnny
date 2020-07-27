9e33c7d3accdae21c2931dfc807b8da0ae473981
arcturus5340 <arcturus5340@gmail.com>
2020-07-27 23:40:34 +0200

    update models
    Models have been edited for easy storage of information
    and better display in the admin panel

--- group/models.py
+++ group/models.py
@@ -7,17 +7,55 @@ class Blacklist(models.Model):
         verbose_name='Ссылка'
     )
 
+    def __str__(self):
+        return 'Нежелательная ссылка ({})'.format(self.link)
+
     class Meta:
         verbose_name = 'Черный список ссылок'
         verbose_name_plural = 'Черный список ссылок'
 
 
-class Members(models.Model):
-    user = models.IntegerField(
-        verbose_name='ID Пользователя'
+class Gratitudes(models.Model):
+    word = models.CharField(max_length=32)
+
+    def __str__(self):
+        return 'Слово благодарности'
+
+    class Meta:
+        verbose_name = 'Список слов благодарности'
+        verbose_name_plural = 'Список слов благодарности'
+
+
+class Groups(models.Model):
+    id = models.IntegerField(
+        verbose_name='ID Группы',
+        primary_key=True,
+        unique=True,
     )
-    chat = models.IntegerField(
-        verbose_name='ID Группы'
+    title = models.CharField(
+        max_length=32,
+        verbose_name='Название Группы',
+    )
+
+    class Meta:
+        verbose_name = 'Группа'
+        verbose_name_plural = 'Группы'
+
+
+class Members(models.Model):
+    id = models.IntegerField(
+        verbose_name='ID Пользователя',
+        primary_key=True,
+        unique=True,
+    )
+    username = models.CharField(
+        max_length=32,
+        verbose_name='Ник Пользователя',
+        blank=True,
+        null=True,
+    )
+    chat_id = models.IntegerField(
+        verbose_name='ID Пользователя',
     )
     date_joined = models.DateTimeField(default=timezone.now)
     rating = models.IntegerField(
@@ -26,7 +64,7 @@ class Members(models.Model):
     )
     mute_rating = models.IntegerField(
         default=0,
-        verbose_name='Кол-во нарушений'
+        verbose_name='Кол-во нарушений',
     )
 
     class Meta:
@@ -35,15 +73,19 @@ class Members(models.Model):
 
 
 class ScheduledMessages(models.Model):
+    id = models.AutoField(primary_key=True)
     chat = models.IntegerField(
-        verbose_name='Группа'
+        verbose_name='Группа',
     )
     text = models.TextField(
-        verbose_name='Текст'
+        verbose_name='Текст',
     )
     interval = models.DurationField(
-        verbose_name='Интервал'
+        verbose_name='Интервал',
     )
+
+    def __str__(self):
+        return 'Запланированное сообщение №{}'.format(self.id)
 
     class Meta:
         db_table = 'group_scheduled_messages'
@@ -53,3 +95,10 @@ class ScheduledMessages(models.Model):
 
 class Swearing(models.Model):
     word = models.CharField(max_length=32)
+
+    def __str__(self):
+        return 'Нецензурное слово'
+
+    class Meta:
+        verbose_name = 'Список нецензурных слов'
+        verbose_name_plural = 'Список нецензурных слов'
