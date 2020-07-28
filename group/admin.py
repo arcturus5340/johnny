3394a58423983f97a6a93b123cc72bb714a85861
arcturus5340 <arcturus5340@gmail.com>
2020-07-28 23:33:39 +0200

    update admin panel view

--- group/admin.py
+++ group/admin.py
@@ -1,3 +1,37 @@
 from django.contrib import admin
+from django.contrib.auth.models import User, Group
 
-# Register your models here.
+from group import models
+from group import forms
+
+@admin.register(models.Blacklist)
+class BlacklistAdmin(admin.ModelAdmin):
+    list_display = ('link', )
+
+
+@admin.register(models.Groups)
+class GroupsAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title')
+
+
+@admin.register(models.Members)
+class MembersAdmin(admin.ModelAdmin):
+    list_display = ('id', 'username', 'rating', 'mute_rating')
+
+
+@admin.register(models.Swearing)
+class SwearingAdmin(admin.ModelAdmin):
+    list_display = ('word', )
+
+
+@admin.register(models.ScheduledMessages)
+class ScheduledMessagesAdmin(admin.ModelAdmin):
+    form = forms.ScheduledMessagesForm
+    list_display = ('chat', 'text', 'interval')
+
+
+admin.site.index_title = 'Админиcтрирование ботов'
+admin.site.site_title = 'Sharewood'
+admin.site.site_header = 'Sharewood Administration'
+admin.site.unregister(Group)
+admin.site.unregister(User)
